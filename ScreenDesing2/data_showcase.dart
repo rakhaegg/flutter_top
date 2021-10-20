6740fa99745564323cd1d5fbae70229328620919
rakhaegg <43262815+rakhaegg@users.noreply.github.com>
2021-10-20 09:58:52 +0700

    Create data_showcase.dart

---
+++ ScreenDesing2/data_showcase.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter_application_1/model/showcase.dart';
+
+  const List<ShowCase> showcases = [
+  ShowCase(
+    image : 'graphic/list1.jpg', 
+    title : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis. ",
+    owner : "Lorem ipsum"
+  ),
+  ShowCase(
+    image : 'graphic/list2.jpg', 
+    title : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis. ",
+    owner : "Lorem ipsum"
+  ),
+  ShowCase(
+   image : 'graphic/list3.jpg', 
+    title : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis. ",
+    owner : "Lorem ipsum"
+  ),
+  ShowCase(
+   image : 'graphic/list4.jpg', 
+    title : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis. ",
+    owner : "Lorem ipsum"
+  ),
+  ShowCase(
+  image : 'graphic/list5.jpg', 
+    title : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis. ",
+    owner : "Lorem ipsum"
+  ),
+  
+];
