795f29bc27fd7c67c44cb73351c943e769853788
rakhaegg <43262815+rakhaegg@users.noreply.github.com>
2021-10-21 13:25:30 +0700

    Create model_showcasew.dart

---
+++ ScreenDesing2/model_showcasew.dart
@@ -0,0 +1,15 @@
+//@dart=2.9
+
+class ShowCase{
+  final String image;
+  final String title;
+  final String owner;
+
+  const ShowCase(
+    {this.image, this.title, this.owner
+    });
+
+ 
+
+   
+}
